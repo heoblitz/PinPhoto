6f9095d562a2f11bc1f27cc6df5c93886a9a6324
wone2287 <wone2287@gmail.com>
2020-06-30 16:23:53 +0900

    init project

---
+++ PinPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinPhoto
+//
+//  Created by won heo on 2020/06/29.
+//  Copyright © 2020 won heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
