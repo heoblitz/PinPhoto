30e97838b8c74875772e5acd22de8750d65a3730
wone2287 <wone2287@gmail.com>
2020-06-30 21:47:02 +0900

    add TabBar Controller & Navigation Header

--- PinPhoto/ViewController.swift
+++ PinPhoto/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.automaticallyAdjustsScrollViewInsets = false
     }
 
 
