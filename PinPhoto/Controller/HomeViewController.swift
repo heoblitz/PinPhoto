e5816c41bb2abdb06890e454746c6f125581c0d2
wone2287 <wone2287@gmail.com>
2020-08-29 14:09:11 +0900

    Remove collection cell spacing method

--- PinPhoto/Controller/HomeViewController.swift
+++ PinPhoto/Controller/HomeViewController.swift
@@ -267,10 +267,6 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
         
         return CGSize(width: width, height: height)
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
 }
 
 // MARK:- ItemObserver Protocol
