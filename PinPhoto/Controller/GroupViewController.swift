0fe6e30491fdd15ee45ab739d87df4441b6fc269
wone2287 <wone2287@gmail.com>
2020-09-03 09:50:58 +0900

    Create Group storyboard & vc

---
+++ PinPhoto/Controller/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  PinPhoto
+//
+//  Created by won heo on 2020/09/01.
+//  Copyright © 2020 won heo. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
