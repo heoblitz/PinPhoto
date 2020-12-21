1324462143f5db393c19675c9ed9a453b2b020ff
heoblitz <wone2287@gmail.com>
2020-12-21 10:59:24 +0900

    Add suggest feature files

---
+++ PinPhoto/Controller/Suggest/SuggestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuggestViewController.swift
+//  PinPhoto
+//
+//  Created by heo on 2020/12/21.
+//  Copyright © 2020 won heo. All rights reserved.
+//
+
+import UIKit
+
+class SuggestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
