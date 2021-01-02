51bef138e95ff6146fd8bb03ee2f817fcfdc67db
heoblitz <wone2287@gmail.com>
2021-01-02 12:43:32 +0900

    Fix suggest cell animation

--- PinPhoto/View/Suggest/SuggestCell.swift
+++ PinPhoto/View/Suggest/SuggestCell.swift
@@ -20,12 +20,38 @@ final class SuggestCell: UICollectionViewCell {
     // MARK:- Methods
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.cornerRadius = 12
-        layer.masksToBounds = true
+        contentView.layer.cornerRadius = 12
+        contentView.layer.masksToBounds = true
     }
     
     func update(by unsplash: Unsplash) {
         suggestNameLabel.text = unsplash.name
         suggestImageView.kf.setImage(with: URL(string: unsplash.thumnail.small))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        animate(isHighlighted: true)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        animate(isHighlighted: false)
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        animate(isHighlighted: false)
+    }
+    
+    private func animate(isHighlighted: Bool) {
+        UIView.animate(withDuration: 0.3) {
+            if isHighlighted {
+                self.contentView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            }
+            else {
+                self.contentView.transform = .identity
+            }
+        }
     }
 }
